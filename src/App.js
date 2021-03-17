ccaa500e81da371608a02035a96ef91454422763
Ploylada Pharikarn <parames.m4xexp@gmail.com>
2021-03-17 09:56:29 +0700

    feat: 🎸 Add pagination and try grid system bootstrap

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Posts from "./components/Posts";
+import Pagination from './components/Pagination'
 import "./App.css";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage] = useState(12);
 
   useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
-      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-      setPosts(res.data);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/photos");
+      setPosts(res.data.slice(0,300));
       setLoading(false);
     };
     fetchPosts();
   }, []);
 
-  console.log(posts);
+  const indexOfTheLastPost = currentPage * postsPerPage;
+  const indexOfTheFirstPost = indexOfTheLastPost - postsPerPage;
+  const currentPost = posts.slice(indexOfTheFirstPost, indexOfTheLastPost);
+
+  //Change page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  }
 
   return (
     <div className="container mt-5">
-      <h1 className="test-primary mb-3">My blog</h1>
-      <Posts posts={posts} loading={loading} />
+      <h1 className="text-primary mb-3 text-center">My blog</h1>
+      <Posts posts={currentPost} loading={loading} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>
     </div>
   );
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,13 +5,30 @@ function posts({ posts, loading }) {
     return <h2 className="loading">Loading...</h2>;
   }
   return (
-    <ul className="list-group mb-4">
-      {posts.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    // <ul className="list-group mb-4">
+    //   {posts.map((post) => (
+    //     <li key={post.id} className="list-group-item">
+    //       {post.title}
+    //     </li>
+    //   ))}
+    // </ul>
+
+    <div className="container">
+      <div class="row">
+        {posts.map((post) => (
+          <div class="col-md-3">  
+            <div key={post.id} className="card mb-4 ">
+              <img className="card-img-top" src={post.thumbnailUrl} alt="Card image cap"></img>
+              <div className="card-body">
+                <h4 className="card-title text-truncate">{post.title}</h4>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               </div>
+            </div>   
+          </div>
+            ))}
+        </div>
+          </div>
+
   );
 }
 
